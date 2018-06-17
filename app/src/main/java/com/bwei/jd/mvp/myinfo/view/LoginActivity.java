cf1532bb525647c54b1bc818b81db00b9bf0ede8
liujiaan <965389477@qq.com>
2018-06-17 23:52:15 +0800

    xinn dai ma

---
+++ app/src/main/java/com/bwei/jd/mvp/myinfo/view/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.bwei.jd.mvp.myinfo.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bwei.jd.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/bwei/jd/mvp/myinfo/view/MyInfoFragment.java
+++ app/src/main/java/com/bwei/jd/mvp/myinfo/view/MyInfoFragment.java
@@ -1,5 +1,6 @@
 package com.bwei.jd.mvp.myinfo.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,16 +8,51 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 import com.bwei.jd.R;
 
-public class MyInfoFragment extends Fragment{
+public class MyInfoFragment extends Fragment implements View.OnClickListener{
+
+    private ImageView myinfo_img;
+    private View view;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.myinfofragment,null);
+        view = inflater.inflate(R.layout.myinfofragment,null);
+
+        initViews();
 
         return view;
+    }
+
+    private void initViews() {
+
+
+        myinfo_img = view.findViewById(R.id.myinfo_img);
+
+        myinfo_img.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.myinfo_img:
+
+                Intent it = new Intent(getActivity(),LoginActivity.class);
+
+                startActivity(it);
+
+                break;
+
+
+        }
+
     }
 }
